ce4c1f0bdbb6942f5c0db3aa9bfe2d7999a1e1d9
Bill Sideris <bill88t@feline.gr>
2024-04-13 17:32:38 +0300

    Update stuff

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@ rm ~/.config/micro/bindings.json || true
 rm ~/.config/micro/settings.json || true
 ln -s ~/git/micro-config/bindings.json ~/.config/micro/bindings.json
 ln -s ~/git/micro-config/settings.json ~/.config/micro/settings.json
+chmod -w ~/.config/micro/bindings.json ~/.config/micro/settings.json
 micro -plugin install wc filemanager jump
 micro -plugin update
