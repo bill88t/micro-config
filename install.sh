3021092af7e2b9d4402f5826b08e583aa683674f
Bill Sideris <bill88t@feline.gr>
2024-04-13 17:28:28 +0300

    Update plugins upon install

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ rm ~/.config/micro/settings.json || true
 ln -s ~/git/micro-config/bindings.json ~/.config/micro/bindings.json
 ln -s ~/git/micro-config/settings.json ~/.config/micro/settings.json
 micro -plugin install wc filemanager jump
+micro -plugin update
