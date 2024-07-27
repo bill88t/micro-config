232acfddbe12df909474fe601525e0bf2083f6c1
Bill Sideris <bill88t@feline.gr>
2024-07-27 20:12:54 +0300

    Aspell

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ ln -s ~/git/micro-config/bindings.json ~/.config/micro/bindings.json
 ln -s ~/git/micro-config/settings.json ~/.config/micro/settings.json
 chmod +x ro.sh rw.sh
 ./ro.sh
-micro -plugin install wc filemanager jump gotham-colors
+micro -plugin install wc filemanager jump gotham-colors aspell
 micro -plugin update
